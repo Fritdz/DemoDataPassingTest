3184c03a9c2058d8e8e6deebf059eaf049b70da0
Fritdz <hafidzzz@hotmail.sg>
2021-05-25 11:03:21 +0800

    Final

--- app/src/main/java/sg/edu/rp/c346/id20033454/demodatapassingtest/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20033454/demodatapassingtest/MainActivity.java
@@ -2,14 +2,18 @@ package sg.edu.rp.c346.id20033454.demodatapassingtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnPassInteger;
+    Button btnPassChar;
+    TextView tvPassDouble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnPassInteger=findViewById(R.id.buttonPassInteger);
+        btnPassChar=findViewById(R.id.buttonPassChar);
+        tvPassDouble=findViewById(R.id.tvPass);
 
         btnPassInteger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+
+        btnPassChar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra("value", 'a');
+                startActivity(intent);
+            }
+        });
+
+        tvPassDouble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FourthActivity.class);
+                intent.putExtra("value", 99.99);
                 startActivity(intent);
             }
         });
